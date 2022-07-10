27bf0965df18a3dc34493dc38a7dd7d204a3f09c
Mason McCoy <mmasonmccoy@gmail.com>
2022-07-10 17:59:18 -0500

    Created login portal that allows existing users to enter valid credentials, authorizing them to access their dashboard (to be created) and view existing blog posts on the homepage. Logout functionality added as well.

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { BlogPost, User } = require('../models');
+const auth = require('../utils/auth');
 
+// Renders homepage with all existing blog posts
 router.get('/', async (req, res) => {
     try {
      
@@ -16,14 +18,16 @@ router.get('/', async (req, res) => {
       const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
   
       res.render('homepage', { 
-        blogPosts
+        blogPosts,
+        logged_in: req.session.logged_in 
       });
     } catch (err) {
       res.status(500).json(err);
     }
   });
 
-router.get('/blogpost/:id', async (req, res) => {
+// Select and view individual blog posts
+router.get('/blogpost/:id', auth, async (req, res) => {
     try {
       const blogPostData = await BlogPost.findByPk(req.params.id, {
         include: [
@@ -37,11 +41,43 @@ router.get('/blogpost/:id', async (req, res) => {
       const blogPost = blogPostData.get({ plain: true });
   
       res.render('blogpost', {
-        ...blogPost
+        ...blogPost,
+        logged_in: req.session.logged_in
       });
     } catch (err) {
       res.status(500).json(err);
     }
+});
+
+// View dashboard of logged in user
+router.get('/dashboard', auth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: BlogPost }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// Render login screen
+router.get('/login', (req, res) => {
+    // Redirect if already logged in
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
 });
 
 module.exports = router;
